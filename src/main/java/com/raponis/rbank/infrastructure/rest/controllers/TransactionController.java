986a638931b063350e19e67fa5023a491da9517b
rapon1kt <shinnatensei@gmail.com>
2025-04-18 18:12:26 -0300

    refactor: change path / path variables and change type of response

--- src/main/java/com/raponis/rbank/infrastructure/rest/controllers/TransactionController.java
+++ src/main/java/com/raponis/rbank/infrastructure/rest/controllers/TransactionController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +26,19 @@ public class TransactionController {
   @Autowired
   public TransactionService transactionService;
 
-  @PostMapping
-  public Transaction releaseNewTransaction(@RequestBody Transaction transaction) {
-    return this.transactionService.releaseNewTransaction(transaction);
+  @PostMapping("/new/{accountOriginId}/to/{accountDestinyId}")
+  public ResponseEntity<Transaction> releaseNewTransaction(@RequestBody Transaction transaction,
+      @PathVariable("accountOriginId") String accountOriginId,
+      @PathVariable("accountDestinyId") String accountDestinyId) {
+    try {
+      Transaction newTransaction = transactionService.releaseNewTransaction(transaction, accountOriginId,
+          accountDestinyId);
+      return new ResponseEntity<>(newTransaction, HttpStatus.CREATED);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    } catch (RuntimeException e) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
   }
 
   @GetMapping("/{id}")
